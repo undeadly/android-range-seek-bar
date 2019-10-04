652644a61780489566891785653c36347d58dff1
Cory Roy <cory.roy@masimo.com>
2019-10-04 16:45:31 -0700

    Fix unit test

--- rangeseekbar/src/test/java/org/florescu/android/rangeseekbar/RangeSeekBarTest.java
+++ rangeseekbar/src/test/java/org/florescu/android/rangeseekbar/RangeSeekBarTest.java
@@ -1,19 +1,22 @@
 package org.florescu.android.rangeseekbar;
 
+import android.os.Build;
+
+import androidx.test.core.app.ApplicationProvider;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.RobolectricGradleTestRunner;
-import org.robolectric.RuntimeEnvironment;
+import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-@RunWith(RobolectricGradleTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = 21)
+@RunWith (RobolectricTestRunner.class)
+@Config (sdk = Build.VERSION_CODES.P)
 public class RangeSeekBarTest {
 
     @Test
     public void rsb_should_handle_long_values() {
-        RangeSeekBar<Long> mSeekBar = new RangeSeekBar<>(RuntimeEnvironment.application);
+        RangeSeekBar<Long> mSeekBar = new RangeSeekBar<>(ApplicationProvider.getApplicationContext());
         // Set up the seek bar
         mSeekBar.setRangeValues(0L, 100L);
         long minValue = mSeekBar.getAbsoluteMinValue();
